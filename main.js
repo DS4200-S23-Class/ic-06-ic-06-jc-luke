d5dede8c0920806ce51e2f74d2d68e2ba1102237
Luke Abbatessa <abbatessa.l@northeastern.edu>
2023-02-13 15:19:39 -0500

    Tried to add event listener

--- main.js
+++ main.js
@@ -2,7 +2,15 @@
 function buttonClicked() {
 	let newText = "Button was clicked!";
 
-	let buttonDiv = document.getElementById("button-div")
+	let buttonDiv = document.getElementById("button-div");
 
 	buttonDiv.innerHTML = newText;
 }
+
+function button2Clicked() {
+	let line = document.getElementByID("line");
+
+	line.classList.add("red");
+}
+
+document.getElementByID("subButton").addEventListener('click', button2Clicked);
